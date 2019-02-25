0674726da9e831ecf0a5b9dbf6a806f02b506b96
Ilya Kolebayev <kolebayev@gmail.com>
2019-02-25 20:18:22 +0300

    Added validation for target files to exist

--- app/index.js
+++ app/index.js
@@ -1,110 +1,38 @@
 const fs = require('fs')
 const path = require('path')
-const folderPath = './_src'
+const readline = require('readline');
 const template = require('./template')
+
+const folderPath = './_src'
 const fileType = '.svg'
 const exportFileType = '.post.css'
-// require('./template')
-// fs.readdirSync(folderPath)
-// console.log(fs.readdirSync(folderPath))
 
-// const Filehound = require('filehound');
-// Filehound.create()
-//   .ext('svg')
-//   .paths(folderPath)
-//   .find((err, svgFiles) => {
-//     if (err) return console.error("handle err", err);
-//
-//     console.log(svgFiles);
-// });
 
 function getFilesFromPath(path, extension) {
     let dir = fs.readdirSync(path);
     return dir.filter( elm => elm.match(new RegExp(`.*\.(${extension})`)));
 }
 
-// console.log(template(svgFilesList[1], fileType));
-
-// var svgFilesList = fs.readdirSync(folderPath);
 var svgFilesList = getFilesFromPath(folderPath, fileType);
-// var data = templ('real', 'talk');
-// console.log(svgFilesList);
-// var data = template.zaz();
-// var svgName = svgFilesList;
-for (i in svgFilesList) {
-  svgFilesList[i] = svgFilesList[i].replace(fileType, '');
 
-  // var data = "." + svgFilesList[i] + " {\n\tbackground-image: url('" + svgFilesList[i] + fileType + "')\n}\n";
-  var data = template(svgFilesList[i], fileType);
-  // var data = require('./powerlevel')(9050);
-  // console.log(template(svgFilesList[i], fileType));
-  // console.log(templ(svgFilesList[i], fileType));
-
-  fs.writeFile(folderPath + "/" + svgFilesList[i] + exportFileType, data, function(err) {
-    if(err) {
-        return console.log(svgFilesList[i] + fileType + " was not written");
-    }
-    console.log("The file " + svgFilesList[i] + exportFileType + " created!");
-  });
+if (svgFilesList.length === 0) {
+  console.log ("No " + fileType + " found in working directory");
+  process.exit();
 }
 
+for (i in svgFilesList) {
+  svgFilesList[i] = svgFilesList[i].replace(fileType, '');
+  var data = template(svgFilesList[i], fileType);
+  fs.writeFile(folderPath + "/" + svgFilesList[i] + exportFileType, data, function(err) {
+    if(err) {
+        return console.log(svgFilesList[i] + fileType + " was not written.");
+    }
+  });
+    console.log("The file " + svgFilesList[i] + exportFileType + " created.");
+}
 
-
-//[ '4glaza.svg', 'ride.svg' ]
-
-
-// console.log(svgFilesList);
-// console.log(typeof svgFilesList);
-// console.log(typeof template, template);
-// console.log(Object.keys(template).map(function(key){ return template[key] }));
-// var aja = Object.keys(template).map(function(key){ return template[key] });
-// console.log(template.data);
-// console.log(svgName);
-
-// var g = 10;
-// template.kek(g);
-
-// var stream = fs.createWriteStream("my_file.txt");
-// var stream = fs.createWriteStream("my_file.txt");
-// stream.once('open', function(fd) {
-//   stream.write("My first row\n");
-//   stream.write("My second row\n");
-//   stream.end();
-// });
-
-// fs.readdir(folderPath, function(err, items) {
-//     console.log(items);
-//
-//     for (var i=0; i<items.length; i++) {
-//         console.log(items[i]);
-//     }
-// });
-
-// const calc = require('./calc')
-// const numbersToAdd = [
-//     3,
-//     4,
-//     10,
-//     2
-// ]
-//
-// const result = calc.sum(numbersToAdd)
-// console.log(`The result is: ${result}`)
-//
-// // const fs = require('fs')
-// // let content
-// // try {
-// //     content = fs.readFileSync('file.md', 'utf-8')
-// // } catch (ex) {
-// //     console.log(ex)
-// // }
-// // console.log(content)
-//
-// const fs = require('fs')
-// fs.readFile('file.md', 'utf-8', function (err, content) {
-//     if (err) {
-//         return console.log(err)
-//     }
-//
-//     console.log(content)
-// })
+if (svgFilesList.length > 1) {
+  console.log('=================================\n' + svgFilesList.length + " files were processed. " + 'Can be found here: ' + folderPath);
+} else {
+  console.log('=================================\n' + svgFilesList.length + " file was processed. " + 'Can be found here: ' + folderPath);
+}
--- app/template.js
+++ app/template.js
@@ -1,54 +1,5 @@
-
-// var template = "ab+c";
-// var lol = ".${y} {\n\tbackground-image: url('${y}.svg')\n}\n";
-// var kek = function (y) {
-//   y = y*y;
-//   console.log(y);
-// }
-
-// var lol = ".brand-logo_name_simtrevel {\n\tbackground-image: url('brand-logo_name_simtrevel.svg')\n}\n"
-
-// var zaz = function (y) {
-  // o = "." + y + "{\n\tbackground-image: url('" + y + ".svg')\n}\n";
-  // o = ".${y} {\n\tbackground-image: url('${y}.svg')\n}\n"
-// }
-// var svgFilesList = [];
-// var svgName = '';
-// var data = "." + svgName + " {\n\tbackground-image: url('" + svgName + ".svg')\n}\n";
-
-//
-// function template (svgName) {
-//   return "." + svgName + " {\n\tbackground-image: url('" + svgName + ".svg')\n}\n";
-// }
-//
-// module.exports.template = template
-//
-
-// function getFilesFromPath(path, extension) {
-//     let dir = fs.readdirSync(path);
-//     return dir.filter( elm => elm.match(new RegExp(`.*\.(${extension})`)));
-// }
-
 function template(name, extension) {
     var x = "." + name + " {\n\tbackground-image: url('" + name + extension + "')\n}\n";
     return x;
 }
 module.exports = template;
-// var data = "." + svgFilesList[i] + " {\n\tbackground-image: url('" + svgFilesList[i] + fileType + "')\n}\n";
-
-// module.exports = templ;
-// module.exports.template = template;
-// module.exports = {
-     // templ: templ
-    // template : template,
-    // kek : kek,
-    // lol : lol,
-    // zaz : zaz,
-    // data : data,
-    // svgName : svgName
-    // svgFilesList : svgFilesList
-// };
-
-// .brand-logo_name_simtrevel {
-// 	background-image: url('brand-logo_name_simtrevel.svg')
-// }
