a2326bc19ac0e1d85460ac4776cf59a35e026d93
Ilya Kolebayev <kolebayev@gmail.com>
2019-02-28 21:26:02 +0300

    CLI path input added, entry instructions moved to module

--- app/index.js
+++ app/index.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
 const path = require('path')
-const readline = require('readline');
+const readline = require('readline-sync')
+const os = require('os')
 const template = require('./template')
+const entryInstructions = require('./entryInstructions')
 
-const importFolderPath = './_src'
 const importFileType = '.svg'
 const exportFileType = '.post.css'
+
+entryInstructions();
+
+const importFolderPath = os.homedir() + '/desktop/' + readline.question('Enter import folder path: ')
+const exportFolderPath = os.homedir() + '/desktop/' + readline.question('Enter export folder path: ')
+console.log('\n');
 
 function getFilesFromPath(path, extension) {
     let dir = fs.readdirSync(path);
@@ -25,16 +32,16 @@ for (i in importFilesList) {
 
   var data = template(importFilesList[i], importFileType);
 
-  fs.writeFile(importFolderPath + "/" + importFilesList[i] + exportFileType, data, function(err) {
+  fs.writeFileSync(exportFolderPath + '/' + importFilesList[i] + exportFileType, data, function(err) {
     if(err) {
-        return console.log(importFilesList[i] + exportFileType + " was not written.");
+        return console.log('\n' + importFilesList[i] + exportFileType + " was not written.");
     }
   });
-    console.log("The file " + importFilesList[i] + exportFileType + " created.");
+  console.log("The file " + importFilesList[i] + exportFileType + " created.");
 }
 
 if (importFilesList.length > 1) {
-  console.log('=================================\n' + importFilesList.length + " files were processed. " + 'Can be found here: ' + importFolderPath);
+  console.log('\n=================================\n\n' + importFilesList.length + " files were processed. \n" + 'Can be found here: ' + exportFolderPath);
 } else {
-  console.log('=================================\n' + importFilesList.length + " file was processed. " + 'Can be found here: ' + importFolderPath);
+  console.log('=================================\n\n' + importFilesList.length + " file was processed. \n" + 'Can be found here: ' + exportFolderPath);
 }
