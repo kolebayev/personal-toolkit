eccd1260e03a20b4b0a158a24738b8f2aac41452
Ilya Kolebayev <kolebayev@gmail.com>
2019-06-14 13:54:25 +0300

    Add colorDiffCalc

---
+++ colordiffcalc/hex2rgb.js
@@ -0,0 +1,24 @@
+module.exports = function hex2rgb(hex) {
+
+    const match = hex.toString(16).match(/[a-f0-9]{6}|[a-f0-9]{3}/i);
+
+    if (!match) {
+        return [0, 0, 0];
+    }
+
+    let colorString = match[0];
+
+    if (match[0].length === 3) {
+        colorString = colorString.split('').map(char => {
+            return char + char;
+        }).join('');
+    }
+
+    const integer = parseInt(colorString, 16);
+    const r = (integer >> 16) & 0xFF;
+    const g = (integer >> 8) & 0xFF;
+    const b = integer & 0xFF;
+
+    return [r, g, b];
+
+}
--- colordiffcalc/index.js
+++ colordiffcalc/index.js
@@ -1,7 +1,27 @@
 module.exports = {
 
     run: function() {
-        console.log('Color diff calculator started\n\n')
-    }
 
+        console.log('Color diff calculator started\n\n')
+        const readlineSync = require('readline-sync')
+        
+        const hex2rgb = require('./hex2rgb')
+        const rgb2hsl = require('./rgb2hsl')
+
+        let hex1 = readlineSync.question('what\'s the 1st HEX code? ');
+        let hex2 = readlineSync.question('what\'s the 2d HEX code? ');
+        let hsl1 = rgb2hsl(hex2rgb(hex1))
+        let hsl2 = rgb2hsl(hex2rgb(hex2))
+
+        let diff = new Object()
+        diff['h'] = hsl1[0] - hsl2[0];
+        diff['s'] = hsl1[1] - hsl1[1] + '%';
+        diff['l'] = hsl1[2] - hsl2[2] + '%';
+
+        console.log('\n');
+        console.log('Diff between ' + hex1 + ' and ' + hex2 + ':' );
+        console.log(diff);
+        console.log('\n');
+
+    }
 };
