be69275dee1549c099b58e5e312f1af0b03b935c
Ilya Kolebayev <kolebayev@gmail.com>
2022-04-16 19:46:14 +0300

    add csv generator

---
+++ csvGenerator/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  run: function () {
+    const csvWriter = require('csv-writer');
+    const casual = require('casual');
+
+    const qty = 1000000;
+
+    const generateUserItem = () => {
+      return {
+        firstName: casual.first_name,
+        lastName: casual.last_name,
+        city: casual.city,
+        phoneNumber: casual.phone,
+        email: casual.email,
+        gender: casual.boolean ? 'male' : 'female',
+      };
+    };
+
+    const countries = [];
+
+    for (let i = 0; i < qty - 1; i++) {
+      let item = generateUserItem();
+      countries.push(item);
+    }
+
+    const writer = csvWriter.createObjectCsvWriter({
+      path: './csvGenerator/data.csv',
+      header: [
+        { id: 'firstName', title: 'First Name' },
+        { id: 'lastName', title: 'Last Name' },
+        { id: 'city', title: 'City' },
+        { id: 'phoneNumber', title: 'phoneNumber' },
+        { id: 'email', title: 'email' },
+        { id: 'gender', title: 'gender' },
+      ],
+    });
+
+    writer.writeRecords(countries).then(() => {
+      console.log('Done!');
+    });
+  },
+};
--- toolbox.js
+++ toolbox.js
@@ -2,38 +2,44 @@
 'use strict';
 
 const prompt = require('prompts');
-const postCssTemplateCreator = require("./pctc/index.js");
-const colorDifferenceCalculator = require("./colordiffcalc/index.js");
+const postCssTemplateCreator = require('./pctc/index.js');
+const colorDifferenceCalculator = require('./colordiffcalc/index.js');
+const csvGenerator = require('./csvGenerator/index.js');
 let interval;
 
-(async function(){
-    const questions = [
-        {
-            type: 'select',
-            name: 'tool',
-            message: 'Select a tool',
-            choices: [
-              { title: 'post css template creator', value: 'pctc' },
-              { title: 'color diff calculator', value: 'cldc' }
-            ]
-        }
-    ];
-    const answers = await prompt(questions, {onCancel:cleanup, onSubmit:cleanup});
+(async function () {
+  const questions = [
+    {
+      type: 'select',
+      name: 'tool',
+      message: 'Select a tool',
+      choices: [
+        { title: 'post css template creator', value: 'pctc' },
+        { title: 'color diff calculator', value: 'cldc' },
+        { title: 'csv generator', value: 'csvgen' },
+      ],
+    },
+  ];
+  const answers = await prompt(questions, {
+    onCancel: cleanup,
+    onSubmit: cleanup,
+  });
 
-    console.log('\n\n\n');
+  console.log('\n\n\n');
 
-    switch(answers.tool) {
-        case 'pctc':
-            postCssTemplateCreator.run();
-            break;
-        case 'cldc':
-            colorDifferenceCalculator.run();
-            break;
-    }
-
+  switch (answers.tool) {
+    case 'pctc':
+      postCssTemplateCreator.run();
+      break;
+    case 'cldc':
+      colorDifferenceCalculator.run();
+      break;
+    case 'csvgen':
+      csvGenerator.run();
+      break;
+  }
 })();
 
 function cleanup() {
-    clearInterval(interval);
+  clearInterval(interval);
 }
-
